be4ef564e590c13fa5e8f7160a797d04ccfe513e
Chenxi Wu <wuchenxi2013@gmail.com>
2020-11-03 14:22:18 -0500

    n

--- composite.py
+++ composite.py
@@ -1,6 +1,6 @@
 from math import *
 f=lambda x : (1-x*x)**0.5
-true_value=0.5*0.75**0.5+pi/6
+true_value=pi/2
 def composite_trapezium(n, a, b, f):
     r=0
     r+=0.5*(f(a)+f(b))
@@ -16,8 +16,8 @@ def composite_simpsons(n, a, b, f):
         r+=2*f(((n-i)*a+i*b)/n)
     return r*(b-a)/3/n
 for n in range(2, 31):
-    result=composite_trapezium(n, -0.5, 0.5, f)
-    print(n, result, log(abs(true_value-result))/log(n))
+    result=composite_trapezium(n, -1, 1, f)
+    print("("+str(log(n))+","+str(-log(abs(true_value-result)))+")")
 for n in range(4, 31, 2):
-    result=composite_simpsons(n, -0.5, 0.5, f)
-    print(n, result, log(abs(true_value-result))/log(n))
+    result=composite_simpsons(n, -1, 1, f)
+    print("("+str(log(n))+","+str(-log(abs(true_value-result)))+")")
